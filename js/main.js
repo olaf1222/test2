9d88d9fc2b2b229f5a976bbe4425e1df7cc1f0cf
{shu} <{shu.tochihara@gmail.com}>
2022-07-19 21:38:05 +0900

    commit1

---
+++ js/main.js
@@ -0,0 +1,85 @@
+'use strict';
+{
+// let apple = 'りんご';
+// console.log(apple);
+
+// let weight = 70;
+// weight = 60;
+// console.log(weight);
+
+// const twice = num =>{
+//  return num*2
+// }
+//   console.log(twice(3));
+
+// function twice(num){
+//   return num*2
+// };
+// console.log(twice(3));
+
+// const addOne = num =>{
+//   return num + 1
+// }
+// console.log(addOne(3));
+
+// const minusOne = num =>{
+//   return num - 1
+// }
+// console.log(minusOne(5));
+
+//7
+// const getLength = str =>{
+//   return str.length
+// }
+// console.log(getLength('apple'));
+
+//9
+// const judgeLength = str =>{
+//   if(str.length<10){
+//       return true
+//   }else{
+//       return false
+//   }
+// }
+// console.log(judgeLength('apple'));
+
+ //10
+// const judgeLength = str =>{
+//   if(str.length >= 5 && str.length < 10){
+//       return true
+//   }else{
+//       return false
+//   }
+// }
+// console.log(judgeLength('appleapple'));
+
+//11
+// const isString = parameter =>{
+//   if(typeof parameter === 'string'){
+//     return true
+//   }
+//   else{
+//     return false
+//   }
+// }
+// console.log(isString(3));
+
+//13
+// const getSum = (num1, num2) => {
+//   return num1 + num2;
+// }
+// console.log(getSum(3,2));
+
+//14
+// const getFullName = (firstName,lastName) => {
+//   return firstName + lastName
+// }
+// console.log(getFullName('Tnaka','Taro'));
+
+//22
+let answer = 'あいうえお';
+answer = 'あいうえおかきくけこ'
+console.log(answer);
+
+
+}
